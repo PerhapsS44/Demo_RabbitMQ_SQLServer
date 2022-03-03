fccbf29825933b9ce55d2d19108a89097e2dee2f
PerhapsS44 <stefan.saraev@yahoo.com>
2022-03-03 16:20:54 +0200

    Am adaugat o interfata pentru un logger si o implementare sub forma de singleton

---
+++ Logger/Logger/Logger.cs
@@ -0,0 +1,71 @@
+﻿using LoggerTemplate;
+using System.Text;
+
+namespace Logger
+{
+    public sealed class Logger : LoggerInterface
+    {
+        private static Logger instance = null;
+        private static readonly object padlock = new object();
+
+        private FileStream logFile;
+        private string fileName;
+        private Logger()
+        {
+        }
+        public static Logger Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Logger();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void SetLogFile(string fileName)
+        {
+            if (fileName != null)
+            {
+                logFile.Close();
+            }
+            this.fileName = fileName;
+
+            logFile = new FileStream(instance.fileName, FileMode.Append);
+
+        }
+
+        public void LogError(string message)
+        {
+            AppendTextToFile($"[error] {message}");
+            Console.WriteLine($"[error] {message}");
+        }
+
+        public void LogException(Exception ex)
+        {
+            AppendTextToFile($"[exception] {ex.Message}");
+        }
+
+        public void LogInfo(string message)
+        {
+            AppendTextToFile($"[info] {message}");
+        }
+
+        public void LogWarning(string message)
+        {
+            AppendTextToFile($"[warning] {message}");
+        }
+
+        private void AppendTextToFile(string message)
+        {
+            ReadOnlySpan<byte> toBeWritten = new ReadOnlySpan<byte>(Encoding.ASCII.GetBytes(message + "\n"));
+            logFile.Write(toBeWritten);
+            logFile.Flush();
+        }
+    }
+}
---
+++ LoggerInterface/LoggerInterface/LoggerInterface.cs
@@ -0,0 +1,11 @@
+﻿namespace LoggerTemplate
+{
+    public interface LoggerInterface
+    {
+        void LogError(string message);
+        void LogWarning(string message);
+        void LogInfo(string message);
+        void LogException(Exception ex);
+
+    }
+}
