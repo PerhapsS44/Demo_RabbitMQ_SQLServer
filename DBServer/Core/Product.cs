522b52d399a6745ab0a00a5e95d400e91d0bc65f
PerhapsS44 <stefan.saraev@yahoo.com>
2022-03-07 14:31:49 +0200

    am modificat si clientul ca sa respecte noile abstractizari

--- DBServer/Core/Product.cs
+++ DBServer/Core/Product.cs
@@ -8,6 +8,12 @@ namespace Core
 {
     public class Product
     {
+        public Product(string name, int quantity, decimal price)
+        {
+            this.name = name;
+            this.quantity = quantity;
+            this.price = price;
+        }
         public string name { get; set; }
         public int quantity { get; set; }
         public decimal price { get; set; }
